adb7f33a683e6a6073f1a2ac54c5f42dc43acb41
ccxla <alexander.seelig@st.ovgu.de>
2015-11-11 18:25:04 +0100

    added stanford parser setup

--- src/textparser/textparser.py
+++ src/textparser/textparser.py
@@ -1,9 +1,13 @@
+import os
 import nltk
 from nltk.corpus import brown
+from nltk.parse.stanford import StanfordParser
+
+testTrees=False
 
 class TextParser():
     def __init__(self):
-        sent = "Thist is a sample sentence, nothing important here."
+        sent = "This is a sample sentence, nothing important here."
         self.tokens = nltk.word_tokenize(sent)
 
         brown_tagged_sents = brown.tagged_sents(categories="news")
@@ -13,3 +17,21 @@ class TextParser():
 
     def output(self):
         return str(self.bigram_tagger.tag(self.tokens)).strip('[]')
+
+if testTrees:
+    # The Stanford Parser is required, download from http://nlp.stanford.edu/software/lex-parser.shtml and unpack somewhere
+    # insert path to java.exe
+    os.environ['JAVAHOME'] = 'C:/Program Files (x86)/Java/jdk1.8.0_60/bin/java.exe'
+    # insert path to the directory containing stanford-parser.jar and stanford-parser-3.5.2-models.jar
+    english_parser = StanfordParser('C:/Python34/Lib/site-packages/stanford-parser-full-2015-04-20/stanford-parser.jar', 'C:/Python34/Lib/site-packages/stanford-parser-full-2015-04-20/stanford-parser-3.5.2-models.jar')
+    s = "I shot an elephant in my pajamas."
+
+    sentences = english_parser.raw_parse_sents((s,))
+    print(sentences) #only prints <listiterator object> in this version
+
+
+    # display the tree
+    for line in sentences:
+        for sentence in line:
+            sentence.draw()
+            brown_tagged_sents = brown.tagged_sents(categories="news")
