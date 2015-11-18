b927625e033a81e5a6221371b94ad3a1432f7cfd
ccxla <alexander.seelig@st.ovgu.de>
2015-11-18 11:42:59 +0100

    added sentence length function

--- src/main.py
+++ src/main.py
@@ -5,11 +5,18 @@ from PyQt5.QtWidgets import QApplication
 from textparser.textparser import *
 from ui.testqt import MainApplication
 
+test_functions = True
+
 # setup nltk text parsing
 textp = TextParser()
-
 standford_parser = Stanford()
-standford_parser.get_sent_depth("I don't even know why this is so deep.", False)
+
+# function tests
+if test_functions:
+    test_sentence = "Why is the rum gone?"
+    print(test_sentence)
+    textp.get_sent_length(test_sentence)
+    standford_parser.get_sent_depth(test_sentence, True)
 
 # setup qt app
 app = QApplication(sys.argv)
--- src/textparser/textparser.py
+++ src/textparser/textparser.py
@@ -4,17 +4,21 @@ from builtins import print
 import nltk
 from nltk.corpus import brown
 from nltk.parse.stanford import StanfordParser
-
+from nltk.tokenize import RegexpTokenizer
 
 class TextParser():
     def __init__(self):
         sent = "This is a sample sentence, nothing important here."
         self.tokens = nltk.word_tokenize(sent)
-
         brown_tagged_sents = brown.tagged_sents(categories="news")
 
         self.unigram_tagger = nltk.UnigramTagger(brown_tagged_sents)
         self.bigram_tagger = nltk.BigramTagger(brown_tagged_sents, backoff=self.unigram_tagger)
+
+    def get_sent_length(self, s):
+        tokenizer = RegexpTokenizer(r'\w+')
+        length = len(tokenizer.tokenize(s))
+        print("The sentence has ", length, "words.")
 
     def output(self):
         return str(self.bigram_tagger.tag(self.tokens)).strip('[]')
@@ -43,3 +47,6 @@ class Stanford():
                 if draw_tree:
                     sentence.draw()
 
+    def get_sent_nomins(self, s):
+
+        return
