9f8ce1ac0b50be8d120deeebe92dd4574fd5ca3a
ccxla <alexander.seelig@st.ovgu.de>
2015-11-18 11:17:09 +0100

    added tree depth function

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 import sys
+
 from PyQt5.QtWidgets import QApplication
+
+from textparser.textparser import *
 from ui.testqt import MainApplication
-from textparser.textparser import TextParser
 
 # setup nltk text parsing
 textp = TextParser()
+
+standford_parser = Stanford()
+standford_parser.get_sent_depth("I don't even know why this is so deep.", False)
 
 # setup qt app
 app = QApplication(sys.argv)
 frame = MainApplication(textp.output())
 frame.show()
 app.exec_()
-
--- src/textparser/textparser.py
+++ src/textparser/textparser.py
@@ -1,10 +1,10 @@
 import os
-import nltk
 from builtins import print
+
+import nltk
 from nltk.corpus import brown
 from nltk.parse.stanford import StanfordParser
 
-testTrees = False
 
 class TextParser():
     def __init__(self):
@@ -19,21 +19,27 @@ class TextParser():
     def output(self):
         return str(self.bigram_tagger.tag(self.tokens)).strip('[]')
 
-if testTrees:
-    # The Stanford Parser is required, download from http://nlp.stanford.edu/software/lex-parser.shtml and unpack somewhere
-    # insert path to java.exe
-    os.environ['JAVAHOME'] = 'C:/Program Files (x86)/Java/jdk1.8.0_60/bin/java.exe'
-    # insert path to the directory containing stanford-parser.jar and stanford-parser-3.5.2-models.jar
-    english_parser = StanfordParser('C:/Python34/Lib/site-packages/stanford-parser-full-2015-04-20/stanford-parser.jar', 'C:/Python34/Lib/site-packages/stanford-parser-full-2015-04-20/stanford-parser-3.5.2-models.jar')
-    s = "I shot an elephant in my pajamas."
 
-    sentences = english_parser.raw_parse_sents((s,))
-    # print(sentences)  # only prints <listiterator object> in this version
+class Stanford():
+    def __init__(self):
+        # The Stanford Parser is required, download from http://nlp.stanford.edu/software/lex-parser.shtml and unpack somewhere
+        # insert path to java.exe
+        os.environ['JAVAHOME'] = 'C:/Program Files (x86)/Java/jdk1.8.0_60/bin/java.exe'
+        # insert path to the directory containing stanford-parser.jar and stanford-parser-3.5.2-models.jar
+        self.english_parser = StanfordParser(
+            'C:/Python34/Lib/site-packages/stanford-parser-full-2015-04-20/stanford-parser.jar',
+            'C:/Python34/Lib/site-packages/stanford-parser-full-2015-04-20/stanford-parser-3.5.2-models.jar')
 
-    # display the tree
-    for line in sentences:
-        for sentence in line:
-            depth = str(sentence.height() - 1)
-            print('The depth of the parse tree is ' + depth + '.')
-            sentence.draw()
-            brown_tagged_sents = brown.tagged_sents(categories="news")
+    def get_sent_depth(self, s, draw_tree):
+        sentences = self.english_parser.raw_parse_sents((s,))
+        # print(sentences)  # only prints <listiterator object> in this version
+
+        # display the tree
+
+        for line in sentences:
+            for sentence in line:
+                depth = str(sentence.height() - 1)
+                print('The depth of the parse tree is ' + depth + '.')
+                if draw_tree:
+                    sentence.draw()
+
