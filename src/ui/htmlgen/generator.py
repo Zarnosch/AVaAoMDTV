a9e94caa61d66a00db40bbd95764364c0de67713
YsenGrimm <kloeten.wars@googlemail.com>
2015-12-18 19:59:19 +0100

    implemented color scale

--- src/ui/htmlgen/generator.py
+++ src/ui/htmlgen/generator.py
@@ -65,11 +65,11 @@ class ViewGenerator(object):
         for sent in text.Sentences:
             data_set = {}
             data_set["id"] = sent.id
-            data_set["f1"] = "red"#sent.sent_len
-            data_set["f2"] = "green"#sent.avg_word_len
-            data_set["f3"] = "blue"#sent.voc_complexity
-            data_set["f4"] = "yellow"#sent.depth
-            data_set["f5"] = "pink"#sent.nominals
+            data_set["f1"] = GenScale.get_color(sent.sent_len)
+            data_set["f2"] = GenScale.get_color(sent.avg_word_len)
+            data_set["f3"] = GenScale.get_color(sent.voc_complexity)
+            data_set["f4"] = GenScale.get_color(sent.depth)
+            data_set["f5"] = GenScale.get_color(sent.nominals)
 
             filled_style += Template(ViewGenerator.barebone_css).substitute(data_set)
 
@@ -81,6 +81,29 @@ class ViewGenerator(object):
         with open("../../generated_html/style.css", "w") as f:
             f.write(filled_template)
 
+class GenScale(object):
+
+    light_percent_min = 40
+    light_percent_max = 100
+
+    hsl_template_blue = "hsl(228, 50%, ${blue}%)"
+    hsl_template_red = "hsl(8, 70%, ${red}%)"
+
+    """Returns a string containing the css rgb() color for the given value."""
+    @staticmethod
+    def get_color(value):
+        hsl_filled = ""
+        if value < 0.5:
+            nv = 2*value
+            ligth_percent = (1-nv) * GenScale.light_percent_min + nv * GenScale.light_percent_max
+            hsl_filled = Template(GenScale.hsl_template_blue).substitute(blue=ligth_percent)
+        else:
+            nv = 2*(value-0.5)
+            ligth_percent = (1-nv) * GenScale.light_percent_max + nv * GenScale.light_percent_min
+            hsl_filled = Template(GenScale.hsl_template_red).substitute(red=ligth_percent)
+
+        return hsl_filled
+
 # class SampleText(object):
 #     def __init__(self):
 #         self.Sentences = [SampleSent(0), SampleSent(1)]
@@ -90,14 +113,15 @@ class ViewGenerator(object):
 #     def __init__(self, id):
 #         self.id = id
 #         self.Text = "Construction started in 1963, and the freeway opened on December 18, 1970."
-#         self.sent_len = 0.5
+#         self.sent_len = 0
 #         self.avg_word_len = 0.3
-#         self.voc_complexity = 0.9
-#         self.depth = 0.2
-#         self.nominals = 0.4
+#         self.voc_complexity = 1
+#         self.depth = 0.7
+#         self.nominals = 0.5
 #
 # if __name__ == '__main__':
 #
+#     GenerateColorScale.get_color(0.33333)
 #     stext = SampleText()
 #
 #     ViewGenerator.generate_css(stext)
