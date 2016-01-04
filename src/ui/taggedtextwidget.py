d3cab84cd3b86c1b3a81b8e13710b782dc6ed7c7
DavidB3 <dboegelsack@googlemail.com>
2016-01-04 13:51:24 +0100

    Caching problem solved

--- src/ui/taggedtextwidget.py
+++ src/ui/taggedtextwidget.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtCore, QtWidgets, QtWebKitWidgets
+from PyQt5.QtWebKit import QWebSettings
 from ui.htmlgen.generator import ViewGenerator
 from textparser.textutil.structures import *
 
@@ -25,7 +26,13 @@ class MQTaggedTextWidget(QtWebKitWidgets.QWebView):
 
         # self.load(QtCore.QUrl('file:///'+os.getcwd()+"/generated_html/index.html"))
 
+        QWebSettings.setMaximumPagesInCache(0)
+        QWebSettings.setObjectCacheCapacities(0, 0, 0)
+
     def showData(self, data):
+        QWebSettings.clearMemoryCaches()
+        QWebSettings.clearIconDatabase()            
+ 
         ViewGenerator.generate_document_view(data, self.app)
 
         time.sleep(2)
@@ -33,6 +40,9 @@ class MQTaggedTextWidget(QtWebKitWidgets.QWebView):
         self.load(QtCore.QUrl('file:///'+os.getcwd()+"/generated_html/document_view.html"))
 
     def showDataNoGeneration(self, data):
+        QWebSettings.clearMemoryCaches()
+        QWebSettings.clearIconDatabase()
+
         ViewGenerator.generate_css(data)
         ViewGenerator.generate_html(data)
 
@@ -41,8 +51,14 @@ class MQTaggedTextWidget(QtWebKitWidgets.QWebView):
         self.load(QtCore.QUrl('file:///'+os.getcwd()+"/generated_html/index.html"))
 
     def showLoading(self):
+        QWebSettings.clearMemoryCaches()
+        QWebSettings.clearIconDatabase()
+
         self.load(QtCore.QUrl('file:///'+os.getcwd()+"/resources/loading.html"))
 
     def showDataNoWait(self, data):
+        QWebSettings.clearMemoryCaches()
+        QWebSettings.clearIconDatabase()
+
         ViewGenerator.generate_document_view(data, self.app)
         self.reload()
