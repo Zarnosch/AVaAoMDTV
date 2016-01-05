de8d3c4055c3780b9063d4d375cc59e094f042e3
YsenGrimm <kloeten.wars@googlemail.com>
2016-01-05 19:20:54 +0100

    pushed some debug foo sry

--- src/ui/text_worker.py
+++ src/ui/text_worker.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtCore
 from textparser.textutil.structures import Text
-import random
+#import random
 
 class TextWorker(QtCore.QObject):
 	finished = QtCore.pyqtSignal()
@@ -13,24 +13,26 @@ class TextWorker(QtCore.QObject):
 		super(TextWorker, self).__init__()
 
 	def longRunning(self):
-		self.FinishedText = SampleText() #Text(self.TextToParse)
+		self.FinishedText = Text(self.TextToParse) #SampleText()
 
-		# while self.FinishedText.process_next_token():
-		# 	self.updated.emit(self.FinishedText.ProcessedTokens / self.FinishedText.TokensCount)
+		while self.FinishedText.process_next_token():
+			self.updated.emit(self.FinishedText.ProcessedTokens / self.FinishedText.TokensCount)
 
 		self.finished.emit()
 
-class SampleText(object):
-    def __init__(self):
-        self.Sentences = [SampleSent(0), SampleSent(1), SampleSent(2), SampleSent(3)]
-
-
-class SampleSent(object):
-    def __init__(self, id):
-        self.id = id
-        self.Text = "Construction started in 1963, and the freeway opened on December 18, 1970."
-        self.sent_len = round(random.random(),2)
-        self.avg_word_len = round(random.random(),2)
-        self.voc_complexity = round(random.random(),2)
-        self.depth = round(random.random(),2)
-        self.nominals = round(random.random(),2)
+# class SampleText(object):
+# 	def __init__(self):
+# 		self.Tokens = random.randrange(5,30)
+# 		self.Sentences = []
+# 		for id in range(0,10):
+# 			self.Sentences.append(SampleSent(id))
+#
+# class SampleSent(object):
+#     def __init__(self, id):
+#         self.id = id
+#         self.Text = "Construction started in 1963, and the freeway opened on December 18, 1970."
+#         self.sent_len = round(random.random(),2)
+#         self.avg_word_len = round(random.random(),2)
+#         self.voc_complexity = round(random.random(),2)
+#         self.depth = round(random.random(),2)
+#         self.nominals = round(random.random(),2)
